9e31001a427578f03c9f373e3bba09ef3fe77fbb
DouglasOttoSf <douglas.davila@objectedge.com>
2022-03-10 17:36:58 -0300

    UpdatingSinglePageTest

--- cypress/integration/singlePageTest.spec.js
+++ cypress/integration/singlePageTest.spec.js
@@ -2,21 +2,34 @@
 
 describe('Single Page Test', () => {
     
+    /*
     beforeEach(() => {
         cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html");
     });
+    */
 
-    it('Inputs', () => {  
-        const user = {
+    const user = {
             firstName: "Douglas",
             lastName: "Davila",
             email: "douglas.davila@objectedge.com",
             special: "Vegetarian",
-            phrase: "I, Douglas Davila, wish to buy 3 General Admission tickets. I understand that all ticket sales are final."
+            agreement: "I, Douglas Davila, wish to buy 3 General Admission tickets. I understand that all ticket sales are final.",
+            ticketConfirmation: "Ticket(s) successfully ordered."
         }
-    
-        cy.fillForm(user);
 
+    it('Fill Form', () => {  
+        cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html");
+        cy.fillForm(user);
+    });
+
+    it('Assertion: Agreements', () => {
+        cy.assertAgreement(user);
+        cy.confirmTicket();
+    });
+
+    it('Assertion: Reset Fields', () => {
+        cy.assertReset(user);
+        cy.assertEmpty();
     });
 
 });
--- cypress/support/singlePageTest_func.js
+++ cypress/support/singlePageTest_func.js
@@ -9,8 +9,29 @@ Cypress.Commands.add('fillForm', (user) => {
     cy.get('#publication').check();
     cy.get('#social-media').check();
     cy.get('#requests').type(user.special);
-    cy.get('#general').check();
-    cy.get('p').should('contain', user.phrase);
-    cy.get('#agree').check();
-})
+    cy.get('#general').check(); 
+});
 
+Cypress.Commands.add('assertAgreement', (user) => { 
+    cy.get('p').should('contain', user.agreement);
+});
+
+Cypress.Commands.add('confirmTicket', () => { 
+    cy.get('#agree').check();
+    cy.get('.active').click();
+    //button[contains(text(),"Confirm Tickets")]
+});
+
+Cypress.Commands.add('assertReset', (user) => { 
+    cy.get('.success').should('contain', user.ticketConfirmation);
+});
+
+Cypress.Commands.add('assertEmpty', () => { 
+    cy.get('#first-name').should('contain', '');
+    cy.get('#last-name').should('contain', '');
+    cy.get('#email').should('contain', '');
+    cy.get('#ticket-quantity').should('have.value', '1');
+    cy.get('#publication').should('not.be.checked');
+    cy.get('#social-media').should('not.be.checked');
+    cy.get('#requests').should('contain', '');
+});
